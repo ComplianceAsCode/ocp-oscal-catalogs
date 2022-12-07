6d098f565ea321e81ec8da18001212de862059fd
Ekaterina Nikonova <enikonovad@gmail.com>
2022-12-08 09:59:50 +1100

    feat: Add git actions
    Signed-off-by: Ekaterina Nikonova <enikonovad@gmail.com>

--- scripts/automation/push.sh
+++ scripts/automation/push.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-function travis-branch-commit() {
+function github-branch-commit() {
+    msg "Github ref $GITHUB_REF" 
+    GIT_BRANCH=${GITHUB_REF##*/}
+    msg "Github branch: ($GIT_BRANCH)" 
     local head_ref branch_ref
     head_ref=$(git rev-parse HEAD)
+    git config --global user.email "automation@example.com"
+    git config --global user.name "Automation Bot" 
     if [[ $? -ne 0 || ! $head_ref ]]; then
         err "failed to get HEAD reference"
         return 1
     fi
-    branch_ref=$(git rev-parse "$TRAVIS_BRANCH")
+    branch_ref=$(git rev-parse "$GIT_BRANCH")
     if [[ $? -ne 0 || ! $branch_ref ]]; then
-        err "failed to get $TRAVIS_BRANCH reference"
+        err "failed to get $GIT_BRANCH reference"
         return 1
     fi
     if [[ $head_ref != $branch_ref ]]; then
-        msg "HEAD ref ($head_ref) does not match $TRAVIS_BRANCH ref ($branch_ref)"
+        msg "HEAD ref ($head_ref) does not match $GIT_BRANCH ref ($branch_ref)"
         msg "someone may have pushed new commits before this build cloned the repo"
         return 0
     fi
-    if ! git checkout "$TRAVIS_BRANCH"; then
-        err "failed to checkout $TRAVIS_BRANCH"
+    if ! git checkout "$GIT_BRANCH"; then
+        err "failed to checkout $GIT_BRANCH"
         return 1
     fi
 
@@ -30,46 +35,48 @@ function travis-branch-commit() {
         err "failed to add modified files to git index"
         return 1
     fi
-    if ! git add adjunct-data; then
+    if ! git add data; then
         err "failed to add modified files to git index"
         return 1
     fi
-    # make Travis CI skip this build
-    if ! git commit -m "Travis Build #${TRAVIS_BUILD_NUMBER} [ci skip]"; then
+    if [ -z "$(git status --porcelain)" ]; then 
+        msg "Nothing to commit" 
+        return 0 
+    fi
+    # make Github CI skip this build
+    if ! git commit -m "Autoupdate [ci skip]"; then
         err "failed to commit updates"
         return 1
     fi
-    # add to your .travis.yml: `branches\n  except:\n  - "/\\+travis\\d+$/"\n`
-    if [[ $TRAVIS_BRANCH = main ]]; then
+    if [[ $GIT_BRANCH = main ]]; then
         echo "Version tag: ${VERSION_TAG}" 
         git tag -d "v${VERSION_TAG}"
-        echo "Adding version tag v${VERSION_TAG} to branch $TRAVIS_BRANCH"
-        if ! git tag "v${VERSION_TAG}" -m "Generated version tag from Travis CI build $TRAVIS_BUILD_NUMBER"; then
+        echo "Adding version tag v${VERSION_TAG} to branch $GIT_BRANCH"
+        if ! git tag "v${VERSION_TAG}" -m "Generated version tag from Github"; then
             err "failed to create git tag: v${VERSION_TAG}"
             return 1
         fi
     fi
     
     local remote=origin
-    if [[ $GITHUB_TOKEN ]]; then
-        remote=https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG
-    fi
-    if [[ $TRAVIS_BRANCH != main ]] && [[ $TRAVIS_BRANCH != develop ]]; then
-        msg "not pushing updates to branch $TRAVIS_BRANCH"
+    if [[ $GIT_TOKEN ]]; then
+        remote=https://$GIT_TOKEN@github.com/ComplianceAsCode/ocp-oscal-catalogs
+    if [[ $GIT_BRANCH != main ]] && [[ $GIT_BRANCH != develop ]]; then
+        msg "not pushing updates to branch $GIT_BRANCH"
         return 0
     fi
-    if ! git push --quiet --follow-tags "$remote" "$TRAVIS_BRANCH" ; then
+    if ! git push --quiet --follow-tags "$remote" "$GIT_BRANCH" ; then
         err "failed to push git changes"
         return 1
     fi
 }
 
 function msg() {
-    echo "travis-commit: $*"
+    echo "github-commit: $*"
 }
 
 function err() {
     msg "$*" 1>&2
 }
 
-travis-branch-commit
+github-branch-commit
