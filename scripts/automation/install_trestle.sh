e558c8643d56b40667220cc848ed66b2a33fc745
Ekaterina Nikonova <enikonovad@gmail.com>
2022-12-11 05:26:25 +0300

    feat: Add versioning

--- scripts/automation/install_trestle.sh
+++ scripts/automation/install_trestle.sh
@@ -2,4 +2,4 @@
 
 python3 -m pip install --upgrade pip setuptools
 python3 -m pip install compliance-trestle==1.2.0
-python3 -m pip install python-semantic-release
+python3 -m pip install python-semantic-release==7.31.4
