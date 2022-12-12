967e5c850a4d1d4037eddb681327858ec8027651
Ekaterina Nikonova <enikonovad@gmail.com>
2022-12-12 15:09:43 +1100

    fix: Change remote

--- scripts/automation/update_profile.sh
+++ scripts/automation/update_profile.sh
@@ -17,10 +17,7 @@ else
      echo "Nothing to commit" 
   else
      git commit -m "$COMMIT_TITLE"
-     local remote=origin
-     if [[ $GIT_TOKEN ]]; then
-        remote=https://$GIT_TOKEN@github.com/ComplianceAsCode/ocp-oscal-profiles
-     fi
+     remote=https://$GIT_TOKEN@github.com/ComplianceAsCode/ocp-oscal-profiles
      git push -u "$remote" "catalogs_autoupdate_$GITHUB_RUN_ID"
      echo $COMMIT_BODY
      curl -u AutomationBot:$GIT_TOKEN -X POST -H "Accept: application/vnd.github.v3+json"  https://api.github.com/repos/ComplianceAsCode/ocp-oscal-profiles/pulls -d '{"head":"'"catalogs_autoupdate_$GITHUB_RUN_ID"'","base":"develop","body":"'"$COMMIT_BODY"'","title":"'"$COMMIT_TITLE"'"}'
